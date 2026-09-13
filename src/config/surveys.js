2c34032d5f597d7997fd1e0f43e9f6d4a6e35af2
miked-chf <miked@fenstermaker.com>
2026-09-12 20:53:20 -0500

    Update COMMUNITY_VOICE_LAYER_URL in surveys.js

--- src/config/surveys.js
+++ src/config/surveys.js
@@ -4,7 +4,7 @@
 export const COMMUNITY_VOICE_SURVEY_URL = "https://survey123.arcgis.com/share/7e2786a877054567b584c43c54f01f6e?embed=true"
 export const STAY_ENGAGED_SURVEY_URL = "https://survey123.arcgis.com/share/1898341f07a641699f384b2aa1f4e77a?embed=true"
 
-export const COMMUNITY_VOICE_LAYER_URL = "https://services9.arcgis.com/QClEuaPkoZwU6r3B/arcgis/rest/services/service_5452d00d54064139b46eed9ec013b3b4_form/FeatureServer/0"
+export const COMMUNITY_VOICE_LAYER_URL = "https://services9.arcgis.com/QClEuaPkoZwU6r3B/arcgis/rest/services/survey123_93165a59b3d040468767175441539704_form/FeatureServer/0"
 export const STAY_ENGAGED_LAYER_URL = "https://services9.arcgis.com/QClEuaPkoZwU6r3B/arcgis/rest/services/service_5eaceaddc32740948648c20d709468a7_form/FeatureServer/0"
 
 // For the live response map dot layer (same as Community Voice layer)
